df82da47bdfb71236314c290e4df9bc4e2071402
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-09-19 12:53:39 +0000

    fix: make code shorter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-const wrapper = async (func, cwd) => {
-  try {
-    return await func()
-  } finally {
-    process.chdir(cwd)
-  }
-}
-
 export default (dir, func) => {
   const cwd = process.cwd()
   process.chdir(dir)
   if (func) {
-    return wrapper(func, cwd)
+    return (async () => {
+      try {
+        return await func()
+      } finally {
+        process.chdir(cwd)
+      }
+    })()
   }
 
   return () => process.chdir(cwd)
